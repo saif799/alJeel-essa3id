d073d0f1eeaed659277ba31f60a0ec27129265b5
Saif <faysalbakziz@gmail.com>
2023-11-20 22:22:52 +0100

    another minor change

--- src/components/ourForm.tsx
+++ src/components/ourForm.tsx
@@ -3,7 +3,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import ar from "date-fns/locale/ar";
-import PhoneInput from "react-phone-number-input";
 
 import { Button } from "@/components/ui/button";
 import {
